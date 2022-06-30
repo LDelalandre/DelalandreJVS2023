f7052b5f43fc7c52154c8356ae0dec36072683be
Delalandre <leo.delalandre@cefe.cnrs>
2022-06-30 19:16:41 +0200

    suppressed some comparison ann-per

---
+++ scripts/CSR_comparison_between_guilds.R
@@ -0,0 +1,68 @@
+source("scripts/Packages.R")
+
+ab_fer <- read.csv2("outputs/data/abundance_fertile.csv")
+ab_nat <- read.csv2("outputs/data/abundance_natif.csv")
+
+MEAN_CSR <- read.csv2("outputs/data/Pierce CSR/Traits_mean_sp_per_trtmt_completed.csv",dec=",") %>% 
+  
+  # merge(name_LH,by="Code_Sp") %>% 
+  # relocate(C,S,R) %>% 
+  mutate(C=str_replace(C,",",".") %>% as.numeric())%>% 
+  mutate(S=str_replace(S,",",".") %>% as.numeric())%>% 
+  mutate(R=str_replace(R,",",".") %>% as.numeric())
+
+MEAN_CSR_shallow <- read.csv2("outputs/data/Pierce CSR/Traits_mean_sp_per_trtmt_subset_nat_sab_completed.csv",dec=",") %>% 
+  # merge(name_LH,by="Code_Sp") %>% 
+  # relocate(C,S,R) %>% 
+  mutate(C=str_replace(C,",",".") %>% as.numeric())%>% 
+  mutate(S=str_replace(S,",",".") %>% as.numeric())%>% 
+  mutate(R=str_replace(R,",",".") %>% as.numeric())
+
+
+# CSR ####
+
+variable_names <- list(
+  "G+F" = expression(paste("G"^'+',"F",sep='')),
+  "GU-S" = expression(paste("GU"[S],sep='')) )
+
+variable_labeller <- function(variable,value){
+  return(variable_names[value])
+}
+
+# Natif: only shallow (sandy) soil
+boxplot_CSR_shallow <- MEAN_CSR_shallow %>% 
+  select(code_sp,treatment,LifeHistory,C,S,R) %>%
+  gather(key = score, value = value, -c(code_sp, LifeHistory,treatment)) %>% 
+  # filter(score == "R") %>% 
+  filter(treatment%in% c("Nat","Fer")) %>% 
+  mutate(zone = if_else(treatment == "Fer", "G+F","GU-S")) %>% 
+  ggplot(aes(x=score, y=value, color = LifeHistory))+
+  theme_classic()+
+  ylab("Score (%)")+
+  theme(axis.title.x=element_blank())+
+  geom_boxplot() +
+  # geom_point() +
+  facet_wrap(~zone,labeller=variable_labeller) 
+  
+
+boxplot_CSR_shallow
+
+ggsave("draft/boxplot_CSR.jpg",boxplot_CSR_shallow)
+
+data.anovaCSR <- MEAN_CSR_shallow %>% 
+  select(code_sp,treatment,LifeHistory,C,S,R) %>%
+  # gather(key = score, value = value, -c(code_sp, LifeHistory,treatment)) %>% 
+  filter(treatment%in% c("Nat","Fer"))
+
+options(contrasts=c("contr.treatment","contr.treatment"))
+lmCSR <- lm(R ~ treatment * LifeHistory, data = data.anovaCSR)
+
+par(mfrow=c(2,2)) ; plot(lmCSR) # diagnostic_graphs
+par(mfrow= c(1,1)) ; plot(density(residuals(lmCSR))) # normality_graph
+shapiro.test(residuals(lmCSR)) # normality of residuals
+lmtest::bptest(lmCSR) # homoscedasticity
+lmtest::dwtest(lmCSR)  # non-independence of residuals!
+anova(lmCSR)
+summary(lmCSR)
+
+
